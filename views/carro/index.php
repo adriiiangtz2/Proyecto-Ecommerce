7e46d3d080d8398b550d9564abf32269c9a34a1b
Rafael de Jesus Madrigal Lopez <rafamadrigal9@gmail.com>
2021-10-18 14:13:49 -0500

    Version 1.9.1 Rafael Vista

--- views/carro/index.php
+++ views/carro/index.php
@@ -16,6 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Crear Carro', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Carrito Vista', ['carro/carrito'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
