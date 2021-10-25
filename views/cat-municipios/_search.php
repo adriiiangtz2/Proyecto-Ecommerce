45ff8177fd922c76ed15cca1e1ce5a963d8bdb95
javier12c <85141814+javier12c@users.noreply.github.com>
2021-10-25 13:23:53 -0500

    version 1.1.4 sarao

--- views/cat-municipios/_search.php
+++ views/cat-municipios/_search.php
@@ -16,7 +16,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'mun_id') ?>
 
     <?= $form->field($model, 'mun_fkestado') ?>
-
     <?= $form->field($model, 'mun_municipio') ?>
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
